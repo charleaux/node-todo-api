12b25f2d34c1bad61b323c8498d5e2386f319792
Charles Torry <ctorry@gmail.com>
2018-07-27 16:56:33 -0400

    added tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -312,13 +312,34 @@ describe('POST /users/login', () => {
                 if (err) {
                     return done(err);
                 }
+                expect(res.body).toEqual({});
                 User.findById(users[1]._id)
                 .then((user) => {
-                    expect(user.email).toBe(res.request._data.email);
                     expect(user.tokens.length).toBe(0);
                     done();
                 }).catch((e) => done(e));
             });
         
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                expect(res.body).toEqual({});
+                User.findById(users[0]._id)
+                    .then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    }).catch((e) => done(e));
+            });
     });
 });
